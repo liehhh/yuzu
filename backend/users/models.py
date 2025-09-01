1140cfdfcd108e016d0cb7c45d13687f9847ec48
liehhh <bnachkebia27@gmail.com>
2025-09-02 02:04:47 +0400

    psycopgbinary

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,13 +1,14 @@
-from django.db import models; import secrets
+from django.db import models
+import secrets
 from django.conf import settings
 from .utils import get_week_start
 from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
-    username = None
-    lastfm_username = models.CharField(max_length=64, unique=True)
+    username = None  # remove default username
 
+    lastfm_username = models.CharField(max_length=64, unique=True)
     display_name = models.CharField(max_length=255, blank=True, default="")
     avatar_url = models.URLField(blank=True, null=True)
     auth_token = models.CharField(max_length=128, unique=True, default="", editable=False)
@@ -15,13 +16,13 @@ class UserProfile(AbstractUser):
     USERNAME_FIELD = "lastfm_username"
     REQUIRED_FIELDS: list[str] = []
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         if not self.auth_token:
-            self.auth_token = secrets.token_urlsafe
+            self.auth_token = secrets.token_urlsafe(48)
         if self.lastfm_username:
             self.lastfm_username = self.lastfm_username.strip().lower()
         return super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.lastfm_username
 
