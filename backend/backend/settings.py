084962f16e76cba3c2e6ca606e4f824f614b5a74
liehhh <bnachkebia27@gmail.com>
2025-09-03 13:24:37 +0400

    db back to sqlite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,11 +94,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-        ssl_require=True,  # Render Postgres requires SSL
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
